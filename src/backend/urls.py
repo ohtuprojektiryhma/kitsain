efcb7d6401d6d1a86878692ecf34617d88f8dbde
Kasper Envalds <kasper.envalds@gmail.com>
2024-01-18 12:02:40 +0200

    rest-api

--- src/backend/urls.py
+++ src/backend/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for kitsain_backend project.
+URL configuration for backend project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
@@ -15,9 +15,20 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
+from rest_framework import routers
+
+from backend.backend import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
 
 urlpatterns = [
-    path("polls/", include("polls.urls")),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
 ]
+
+urlpatterns += router.urls
