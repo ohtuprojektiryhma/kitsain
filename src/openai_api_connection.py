50d29f357956ab0113d608977a6daa03a33ab0be
SJET-Code <santeri.tolonen@helsinki.fi>
2024-01-18 15:26:17 +0200

    fix lint

--- src/openai_api_connection.py
+++ src/openai_api_connection.py
@@ -1,6 +1,6 @@
-from openai import OpenAI
 import os
 import json
+from openai import OpenAI
 from dotenv import load_dotenv
 
 dirname = os.path.dirname(__file__)
@@ -21,15 +21,20 @@ class OpenaAI_API_connection:
             messages=[
                 {
                     "role": "system",
-                    "content": """You are a professional kitchen assistant, with access and knowledge about the users pantry, you give recipe suggestions based on these ingredients, 
-                            but don't have to use all of them to make a recipe. You only give responses in a single JSON object that only has the keys in this form: first key = recipe_name, 
-                            second key = ingredients, third key = instructions. The values of the keys should be like this: first value = the name of the recipe we are making, second value = 
-                            a dictionary of the ingredients used in the recipe and the key being the ingredient and value being the amount needed, third value = instructions list with the 
+                    "content": """You are a professional kitchen assistant, with access and knowledge
+                      about the users pantry, you give recipe suggestions based on these ingredients,
+                            but don't have to use all of them to make a recipe. You only give responses
+                            in a single JSON object that only has the keys in this form: first key = recipe_name,
+                            second key = ingredients, third key = instructions. The values of the keys should be
+                            like this: first value = the name of the recipe we are making, second value =
+                            a dictionary of the ingredients used in the recipe and the key being the ingredient
+                            and value being the amount needed, third value = instructions list with the
                             instructions ordered by the order which they are done in.""",
                 },
                 {
                     "role": "user",
-                    "content": f"This is my pantry: {ingredients}. Please give a {recipe_type} recipe I can make with these.",
+                    "content": f"""This is my pantry: {ingredients}. Please give a {recipe_type}
+                    recipe I can make with these.""",
                 },
             ],
         )
