8b01cb5420e9c03dc54191f29ac09eefafcd3965
SJET-Code <santeri.tolonen@helsinki.fi>
2024-01-22 15:22:14 +0200

    added demo CLI version back

--- src/backend/backend/views.py
+++ src/backend/backend/views.py
@@ -4,39 +4,49 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from backend.backend.models import Recipe
 from django.contrib.auth.models import Group, User
-from backend.backend.serializers import RecipeSerializer, UserSerializer, GroupSerializer, IngredientsSerializer, StepsSerializer
+from backend.backend.serializers import (
+    RecipeSerializer,
+    UserSerializer,
+    GroupSerializer,
+    IngredientsSerializer,
+    StepsSerializer,
+)
 from services.recipe_service import RecipeService
+
 
 class RecipeViewSet(viewsets.ModelViewSet):
     """
     API endoint that allows recipes to be viewed or edited.
     """
+
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     permission_classes = [permissions.IsAuthenticated]
+
 
 @csrf_exempt
 @require_POST
 def recipe_generator(request):
     data = request.POST
     print(data)
-    serializer = IngredientsSerializer(data=data['ingredients'])
+    serializer = IngredientsSerializer(data=data["ingredients"])
 
     if serializer.is_valid():
-        ingredients = serializer.validated_data['ingredients']
+        ingredients = serializer.validated_data["ingredients"]
 
-        recipe = RecipeService().get_recipe(ingredients, data['recipe_type'])
+        recipe = RecipeService().get_recipe(ingredients, data["recipe_type"])
 
         return recipe
 
-    return JsonResponse({'error': 'Invalid ingredients data'}, status=400)
+    return JsonResponse({"error": "Invalid ingredients data"}, status=400)
 
-    
+
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
     """
-    queryset = User.objects.all().order_by('-date_joined')
+
+    queryset = User.objects.all().order_by("-date_joined")
     serializer_class = UserSerializer
     permission_classes = [permissions.IsAuthenticated]
 
@@ -45,6 +55,7 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
     """
+
     queryset = Group.objects.all()
     serializer_class = GroupSerializer
     permission_classes = [permissions.IsAuthenticated]
