771c182f77b8485065e7fc08b7201380d93b606f
Tuupertunut <tuupertunut@outlook.com>
2024-01-24 12:18:03 +0200

    Add recipe changing function to openAI service

--- src/openai_api_connection.py
+++ src/openai_api_connection.py
@@ -10,33 +10,57 @@ try:
 except FileNotFoundError:
     pass
 
+GENERATION_MESSAGE = {
+    "role": "system",
+    "content": """You are a professional kitchen assistant, with access and knowledge
+                about the users pantry, you give recipe suggestions based on these ingredients,
+                but don't have to use all of them to make a recipe. You only give responses
+                in a single JSON object that only has the keys in this form: first key = recipe_name,
+                second key = ingredients, third key = instructions. The values of the keys should be
+                like this: first value = the name of the recipe we are making, second value =
+                a dictionary of the ingredients used in the recipe and the key being the ingredient
+                and value being the amount needed, third value = instructions list with the
+                instructions ordered by the order which they are done in.""",
+}
+
 
 class OpenAI_API_connection:
     def __init__(self):
         self.client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+        # current chat session
+        self.messages = []
 
-    def get_recipe_suggestions(self, ingredients: str, recipe_type: str):
+    def send_messages_to_gpt(self):
+        # call openai api
         completion = self.client.chat.completions.create(
             model="gpt-4",
-            messages=[
-                {
-                    "role": "system",
-                    "content": """You are a professional kitchen assistant, with access and knowledge
-                      about the users pantry, you give recipe suggestions based on these ingredients,
-                            but don't have to use all of them to make a recipe. You only give responses
-                            in a single JSON object that only has the keys in this form: first key = recipe_name,
-                            second key = ingredients, third key = instructions. The values of the keys should be
-                            like this: first value = the name of the recipe we are making, second value =
-                            a dictionary of the ingredients used in the recipe and the key being the ingredient
-                            and value being the amount needed, third value = instructions list with the
-                            instructions ordered by the order which they are done in.""",
-                },
-                {
-                    "role": "user",
-                    "content": f"""This is my pantry: {ingredients}. Please give a {recipe_type}
-                    recipe I can make with these.""",
-                },
-            ],
+            messages=self.messages,
         )
-        openai_response = completion.choices[0].message.content
-        return json.loads(openai_response)
+        response = completion.choices[0].message
+
+        # add response to chat session and return it
+        self.messages.append(response)
+        return response
+
+    def get_recipe_suggestions(self, ingredients: str, recipe_type: str):
+        # init chat session
+        self.messages.clear()
+        self.messages.append(GENERATION_MESSAGE)
+        self.messages.append(
+            {
+                "role": "user",
+                "content": f"""This is my pantry: {ingredients}. Please give a {recipe_type}
+                            recipe I can make with these.""",
+            }
+        )
+
+        response = self.send_messages_to_gpt()
+
+        return json.loads(response.content)
+
+    def change_recipe(self, change: str):
+        self.messages.append({"role": "user", "content": change})
+
+        response = self.send_messages_to_gpt()
+
+        return json.loads(response.content)
