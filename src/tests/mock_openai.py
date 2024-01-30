36b7da584d01e59c33d29385b9afc5603f612747
Tuupertunut <tuupertunut@outlook.com>
2024-01-30 12:42:58 +0200

    Add tests for recipe changing

--- src/tests/mock_openai.py
+++ src/tests/mock_openai.py
@@ -2,15 +2,34 @@ import json
 from unittest.mock import MagicMock
 
 
+def create(model: str, messages: list):
+    response = MagicMock()
+
+    # 2 messages means that we are generating new recipe
+    if len(messages) == 2:
+        response.choices[0].message.content = json.dumps(
+            {
+                "recipe_name": "Mock Soup",
+                "ingredients": {"Water": "1 cup", "Salt": "1 tsp"},
+                "instructions": ["Boil water", "Add salt"],
+            }
+        )
+    # more than 2 messages means we are changing a recipe
+    else:
+        response.choices[0].message.content = json.dumps(
+            {
+                "recipe_name": "Improved Mock Soup",
+                "ingredients": {"Water": "1 cup", "Salt": "1 tsp", "Pepper": "1 tsp"},
+                "instructions": ["Boil water", "Add salt", "Add pepper"],
+            }
+        )
+
+    return response
+
+
 class OpenAI:
     def __init__(self, api_key: str):
         pass
 
     chat = MagicMock()
-    chat.completions.create.return_value.choices[0].message.content = json.dumps(
-        {
-            "recipe_name": "Mock Soup",
-            "ingredients": {"Water": "1 cup", "Salt": "1 tsp"},
-            "instructions": ["Boil water", "Add salt"],
-        }
-    )
+    chat.completions.create = create
--- src/tests/test_app.py
+++ src/tests/test_app.py
@@ -27,3 +27,30 @@ class TestApp(TestCase):
                 "instructions": ["Boil water", "Add salt"],
             },
         )
+
+    # Inject mock OpenAI object into the service class.
+    @patch("app.openai_service.client", OpenAI(api_key="mock api key"))
+    def test_change_recipe(self):
+        # Generate new recipe
+        data = {"ingredients": "Water, Salt", "recipe_type": "Soup"}
+
+        response = self.client.post("/generate", json=data)
+
+        self.assertEqual(response.status_code, 200)
+        
+        # Ask for a change
+        data = {"change": "Add pepper"}
+
+        response = self.client.post("/change", json=data)
+
+        self.assertEqual(response.status_code, 200)
+
+        response_data = response.json
+        self.assertEqual(
+            response_data,
+            {
+                "recipe_name": "Improved Mock Soup",
+                "ingredients": {"Water": "1 cup", "Salt": "1 tsp", "Pepper": "1 tsp"},
+                "instructions": ["Boil water", "Add salt", "Add pepper"],
+            },
+        )
