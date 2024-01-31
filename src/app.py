928ffa4bfd51b45d744695ca5186bb4fa0399bde
erz64 <eeroranta111@gmail.com>
2024-01-31 13:03:32 +0200

    made so that the frontend saves recipes to a txt file

--- src/app.py
+++ src/app.py
@@ -3,6 +3,7 @@ import json
 from flask import Flask, request, render_template
 from openai import OpenAI
 from dotenv import load_dotenv
+
 # import db
 from services.openai_service import OpenAIService
 
@@ -42,25 +43,28 @@ def generate_recipe():
         with open("recipes.txt", encoding="utf-8") as f:
             for jsonObj in f:
                 recipeDict = json.loads(jsonObj)
-                recipeDict["ingredients"] = list(
-                    recipeDict["ingredients"].items())
+                recipeDict["ingredients"] = list(recipeDict["ingredients"].items())
                 recipe_list.append(recipeDict)
         return render_template("generate_recipe.html", recipes=recipe_list)
     if request.method == "POST":
-        recipe_type = request.form["recipe_type"]
+        print(request)
+        print(request.content_type)
+        recipe = request.json
+        print(recipe)
+        """        recipe_type = request.form["recipe_type"]
         ingredient_dict = {}
         ingredient_dict["ingredients"] = request.form.getlist("ingredient")
-        ingredient_dict["recipe_type"] = recipe_type
+        ingredient_dict["recipe_type"] = recipe_type"""
         # for ingredient in ingredient_dict["ingredients"]:
         #     db.insert_ingredient(ingredient)
 
-        with open("frontend.json", encoding="utf-8") as f:
-            d = json.load(f)
-            recipe = json.dumps(d)
+        # with open("frontend.json", encoding="utf-8") as f:
+        # d = json.load(f)
+        # recipe = json.dumps(d)
         with open("recipes.txt", "a", encoding="utf-8") as recipes_file:
             recipes_file.write(f"{recipe}\n")
 
-        return recipe
+        return request.json
     return None
 
 
