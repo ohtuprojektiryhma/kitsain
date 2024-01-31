e719da301b9de87283c0c6f8e638011c4b929db6
Atte Högman <atte.hogman@gmail.com>
2024-01-31 15:42:43 +0200

    add entities modify app.py

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 import os
 import json
+from entities import entities
 from flask import Flask, request, render_template
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -20,6 +21,17 @@ openai_service = OpenAIService(OpenAI(api_key=os.getenv("OPENAI_API_KEY")))
 app = Flask(__name__)
 
 
+def create_pantry():
+    ingredient1 = entities.Ingredient("ground beef", "400 g", "6407840041172")
+    ingredient2 = entities.Ingredient("macaroni", "400 g", "6417700050725")
+
+    pantry = entities.Pantry()
+    pantry.add_ingredient(ingredient1)
+    pantry.add_ingredient(ingredient2)
+
+    return pantry.ingredients
+
+
 @app.route("/generate", methods=["POST"])
 def generate():
     request_body = request.json
@@ -38,6 +50,7 @@ def change():
 
 @app.route("/frontend", methods=["GET", "POST"])
 def generate_recipe():
+    pantry = create_pantry()
     if request.method == "GET":
         recipe_list = []
         with open("recipes.txt", encoding="utf-8") as f:
@@ -45,7 +58,9 @@ def generate_recipe():
                 recipeDict = json.loads(jsonObj)
                 recipeDict["ingredients"] = list(recipeDict["ingredients"].items())
                 recipe_list.append(recipeDict)
-        return render_template("generate_recipe.html", recipes=recipe_list)
+        return render_template(
+            "generate_recipe.html", recipes=recipe_list, pantry=pantry
+        )
     if request.method == "POST":
         recipe = request.get_json()
         recipe = json.dumps(recipe)
--- src/entities/entities.py
+++ src/entities/entities.py
@@ -19,12 +19,3 @@ class Ingredient:
 
     def __str__(self):
         return f"{self.name}, {self.amount}: {self.barcode}"
-
-
-ingredient1 = Ingredient("ground beef", "400 g", "6407840041172")
-ingredient2 = Ingredient("macaroni", "400 g", "6417700050725")
-
-pantry = Pantry()
-pantry.add_ingredient(ingredient1)
-pantry.add_ingredient(ingredient2)
-print(pantry.display_contents())
