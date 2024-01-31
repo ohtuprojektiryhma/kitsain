94d2604a5d07e68e7503e6116694b4d164ce00db
erz64 <eeroranta111@gmail.com>
2024-01-31 15:45:27 +0200

    changed so the routes are in route file and app file only initializes the flask app

--- src/routes.py
+++ src/routes.py
@@ -1,5 +1,7 @@
+from app import app
 import os
 import json
+from entities import entities
 from flask import Flask, request, render_template
 from openai import OpenAI
 from dotenv import load_dotenv
@@ -17,8 +19,6 @@ except FileNotFoundError:
 
 openai_service = OpenAIService(OpenAI(api_key=os.getenv("OPENAI_API_KEY")))
 
-app = Flask(__name__)
-
 """Mock recipe generation route for returning a mock recipe
 
 Returns:
@@ -31,6 +31,17 @@ def mock_generate():
     with open("mock_recipe.json", encoding="utf-8") as file:
         mock_recipe = json.load(file)
     return mock_recipe
+
+
+def create_pantry():
+    ingredient1 = entities.Ingredient("ground beef", "400 g", "6407840041172")
+    ingredient2 = entities.Ingredient("macaroni", "400 g", "6417700050725")
+
+    pantry = entities.Pantry()
+    pantry.add_ingredient(ingredient1)
+    pantry.add_ingredient(ingredient2)
+
+    return pantry.ingredients
 
 
 @app.route("/generate", methods=["POST"])
@@ -51,6 +62,7 @@ def change():
 
 @app.route("/frontend", methods=["GET", "POST"])
 def generate_recipe():
+    pantry = create_pantry()
     if request.method == "GET":
         recipe_list = []
         with open("recipes.txt", encoding="utf-8") as f:
@@ -58,7 +70,9 @@ def generate_recipe():
                 recipeDict = json.loads(jsonObj)
                 recipeDict["ingredients"] = list(recipeDict["ingredients"].items())
                 recipe_list.append(recipeDict)
-        return render_template("generate_recipe.html", recipes=recipe_list)
+        return render_template(
+            "generate_recipe.html", recipes=recipe_list, pantry=pantry
+        )
     if request.method == "POST":
         recipe = request.get_json()
         recipe_string = json.dumps(recipe)
