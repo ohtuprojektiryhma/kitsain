7958b118665b402e5a927104c8f48eef29ce0546
Iris <iris.perttuli@gmail.com>
2024-03-13 14:44:00 +0200

    fix schema

--- src/schema.sql
+++ src/schema.sql
@@ -1,10 +1,33 @@
-drop table usertoken;
+drop table pantry;
+
+drop table recipes;
+
+drop table instructions;
+
+drop usertoken;
 
 
-CREATE TABLE IF NOT EXISTS usertoken (
+CREATE TABLE IF NOT EXISTS recipes (
     id SERIAL PRIMARY KEY,
-    token TEXT,
-    usage INT
+    recipe_json json
+);
+
+CREATE TABLE IF NOT EXISTS pantry (
+    id SERIAL PRIMARY KEY,
+    ingredient_name TEXT,
+    amount TEXT,
+    serial_number TEXT
 );
 
 
+CREATE TABLE IF NOT EXISTS instructions (
+    id SERIAL PRIMARY KEY,
+    step_number INTEGER,
+    instruction_text TEXT
+);
+
+CREATE TABLE IF NOT EXISTS usertoken (
+    id SERIAL PRIMARY KEY,
+    token json,
+    usage INT
+);
