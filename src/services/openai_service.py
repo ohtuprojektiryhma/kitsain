4aa4c2c2d0bf1529394e82df9984ce29ea174833
SJET-Code <santeri.tolonen@helsinki.fi>
2024-03-18 15:46:45 +0200

    updated comments and cleaned up code

--- src/services/openai_service.py
+++ src/services/openai_service.py
@@ -1,49 +1,5 @@
 import json
 
-GENERATION_MESSAGE_PO_FULL = {
-    "role": "system",
-    "content": """
-You are a tool that generates recipes in a precise JSON format.
-
-You are given the following information to form the recipe:
-Required items: items that must be used in the recipe, use these items in the recipe no matter what.
-Pantry items: items available in the users pantry, these can be used in the recipe if needed.
-You must not use any other extra ingredients in the recipe, even if the recipe would not make sense.
-Recipe type: type of recipe to be generated.
-Special supplies: special kitchen supplies that could be used to make the recipe.
-Language: language that the recipe should be generated in.
-
-Generate a recipe and respond only precisely in the following JSON format:
-{
-    "recipe_name": name of the generated recipe,
-    "ingredients": {dict where key = ingredient name, and value = 
-    amount needed for the recipe in metric system, with the unit included (ml, g, kg, etc.)},
-    "instructions": [numbered list of instructions on how to make the recipe]
-}
-""",
-}
-GENERATION_MESSAGE_NPO_FULL = {
-    "role": "system",
-    "content": """
-You are a tool that generates recipes in a precise JSON format.
-
-You are given the following information to form the recipe:
-Required items: items that must be used in the recipe, use these items in the recipe no matter what.
-Pantry items: items available in the users pantry, these can be used in the recipe if needed.
-You can also use other extra ingredients in the recipe, if needed.
-Recipe type: type of recipe to be generated.
-Special supplies: special kitchen supplies that could be used to make the recipe.
-Language: language that the recipe should be generated in.
-
-Generate a recipe and respond only precisely in the following JSON format:
-{
-    "recipe_name": name of the generated recipe,
-    "ingredients": {dict where key = ingredient name, and value = 
-    amount needed for the recipe in metric system, with the unit included (ml, g, kg, etc.)},
-    "instructions": [numbered list of instructions on how to make the recipe]
-}
-""",
-}
 CHANGE_MESSAGE = {
     "role": "system",
     "content": """
@@ -76,7 +32,7 @@ class OpenAIService:
             }
         return completion.choices[0].message
 
-    def _form_genaration_message(
+    def __form_generation_message(
         self,
         required_items: list[str],
         pantry: list[str],
@@ -84,6 +40,9 @@ class OpenAIService:
         recipe_type: str,
         special_supplies: list[str],
     ) -> dict:
+        # Internal function that constructs the generation message
+        # according to the content of the sent request
+        # This is a string literal, the indentation is wrong on purpose
         generation_message_content = """
 You are a tool that generates recipes in a precise JSON format.
 
@@ -132,10 +91,10 @@ Generate a recipe and respond only precisely in the following JSON format:
         special_supplies: list[str],
         language: str,
     ) -> dict:
-        # First send instructions in GENERATION_MESSAGE, then user input in a second message.
-        # Use different GENERATION_MESSAGE depending on if pantry_only is selected.
+        # First send instructions from __form_generation_message internal function,
+        # then user input in a second message.
         messages = []
-        generation_message = self._form_genaration_message(
+        generation_message = self.__form_generation_message(
             required_items, pantry, pantry_only, recipe_type, special_supplies
         )
         messages.append(generation_message)
