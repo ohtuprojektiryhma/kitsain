947ee688b57d86b1ccaf2e770aee88520eb2b496
erz64 <eeroranta111@gmail.com>
2024-04-11 14:20:52 +0300

    trying to modify prompt so it makes recipes more diverse

--- src/services/openai_service.py
+++ src/services/openai_service.py
@@ -48,6 +48,8 @@ class OpenAIService:
         generation_message_content = """
 You are an advanced tool designed to create unique, appealing, and tasty recipes in a precise JSON format. Your task is to synthesize a recipe based on the provided parameters, adhering to the following guidelines: (you do not need to use the full amount of ingredient, only use as much as needed for the recipe)
 """
+        generation_message_content += """Good recipes are generally diverse and include most of the macronutrients (eg. proteins, carbohydrates, vitamins, fats), so try to keep this in mind."""
+
         if len(required_items) != 0:
             generation_message_content += """
 Required Items: Incorporate all items listed under "Required items" into the recipe. These ingredients are essential and should be used without exception. If given an amount for the item, only use as much as the amount says.
